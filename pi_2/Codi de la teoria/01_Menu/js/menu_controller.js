e4dd53c812ac882dc9de7d8ea86be930417be196
u1973231 <99675672+u1973231@users.noreply.github.com>
2022-03-08 19:25:09 +0100

    Add files via upload

---
+++ pi_2/Codi de la teoria/01_Menu/js/menu_controller.js
@@ -0,0 +1,17 @@
+function start_game(){
+	name = prompt("User name");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+}
+
+function options(){
+	// TODO: Open options menu
+	console.log("Options menu button");
+}
+
+
---
+++ pi_2/js/menu_controller.js
@@ -0,0 +1,21 @@
+function start_game(){
+	name = prompt("User name");
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	loadpage("../index.html")
+}
+
+function options(){
+	// TODO: Open options menu
+	console.log("Options menu button");
+}
+
+function menuTeoria(){
+	
+	loadpage("./Codi de la teoria/01_Menu/index.html");
+}
+
+
+
